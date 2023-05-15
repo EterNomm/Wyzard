302dcc6b605e7a56f493226594e6da34eebb7566
LyQuid <83483788+LyQuid12@users.noreply.github.com>
2023-05-15 16:44:29 +0700

    Pre-Alpha Initial Files

---
+++ wyzard/__init__.py
@@ -0,0 +1,10 @@
+from .gpt_detector import GPTDetector
+from .object_detection import ObjectDetection
+from .image_to_text import ImageToText
+#from .utils import *
+
+__title__ = "Wyzard"
+__version__ = "0.1b1"
+__authors__ = "EterNomm"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2023-present EterNomm & LyQuid"
