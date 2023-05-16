21ab0eb597e996d4a22c8115c5c88139f2bbc311
LyQuid <83483788+LyQuid12@users.noreply.github.com>
2023-05-16 13:14:20 +0700

    Update image_captioning.py

--- examples/image_captioning.py
+++ examples/image_captioning.py
@@ -1,12 +1,14 @@
 import wyzard
 import requests
+import torch
 
 # Load image from url
 url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 image = requests.get(url, stream=True).raw
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Start and load the model
-detector = wyzard.ImageCaptioning(model="nlpconnect/vit-gpt2-image-captioning")
+detector = wyzard.ImageCaptioning(model="nlpconnect/vit-gpt2-image-captioning", device=device)
 # Predicting the caption
 results = detector.predict(image)
 
