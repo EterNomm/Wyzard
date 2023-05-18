29a1aea9d2efcbe231b0b1f1a56b1c3b6beacb33
LyQuid <83483788+LyQuid12@users.noreply.github.com>
2023-05-18 12:53:33 +0700

    Update `__version__` to `1.0`

--- wyzard/__init__.py
+++ wyzard/__init__.py
@@ -4,7 +4,7 @@ from .image_captioning import ImageCaptioning
 #from .utils import *
 
 __title__ = "Wyzard"
-__version__ = "0.1b1"
+__version__ = "0.1"
 __authors__ = "LyQuid"
 __license__ = "MIT License"
 __copyright__ = "Copyright 2023-present LyQuid"
